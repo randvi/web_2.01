9bfb77cb0e8bf3f94eacb6fd847876e51c3f8838
randvi <6535761@gmail.com>
2017-11-05 15:51:47 +0300

    first commit

---
+++ etc/hello.py
@@ -0,0 +1,18 @@
+#gunicorn configuration file
+
+CONFIG = {
+   'mode': 'wsgi',
+   'working_dir': '/home/box/web',
+    #'python': '/usr/bin/python3',
+    'args': (
+        '--bind=0.0.0.0:8080',
+		'--workers=4',
+        '--timeout=60',
+		'--log-level=debug',
+        'hello:app'
+    )
+}
+
+#pythonpath='/home/box/web'
+#bind='0.0.0.0:8080'
+#workers=4
